16e4129142a218c561ad9003e6f096f3c483d71f
Ira Riyana <irariyanasari@gmail.com>
2024-09-08 13:27:48 +0800

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary60: '#007293',
+        primary50: '#0092AC',
+        textColor: '#667085',
+        borderCard: {
+          textFieldBorder: '#474D66',
+        },
+      },
+      fontSize: {
+        'custom-30': '30px',
+        'custom-16': '16px',
+        'custom-14': '14px',
+        'custom-12': '12px',
+      },
+      fontFamily: {
+        nunito: ['"Nunito Sans"', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
